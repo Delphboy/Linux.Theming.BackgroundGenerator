7b7227aca01902bbbb606aa2c49d80a99983ee56
henry <hps1945@live.co.uk>
2020-08-19 11:58:17 +0100

    added dotnet setup

---
+++ Linux.Theming.BackgroundGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Linux.Theming.BackgroundGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
